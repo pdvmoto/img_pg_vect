651b476a8a5fe4072269b7506b5a6708057c958c
pdvbv macbook <piet.devisser@cheerful.com>
2025-06-26 16:12:03 +0200

    aas

--- duration.py
+++ duration.py
@@ -12,8 +12,10 @@ from datetime import datetime
 # - tmr_total() : get sec since tmr_total, stopwatch from start of program
 # - tmr_set()   : set point to measure, now(), also re-sset.. global_var: g_start_dur
 # - tmr_durat() : get duration in sec/ms since last set, the stopwatch inside the program
+# - tmr_spin()  : spin on full-CPU for nr of n_sec (can be float) - Spinning/running Wait...
 # 
 # todo: 
+# - investigate time vs datetime packages, prefer only 1
 # - use more than 1 timer..so they dont interfere. allow nested timeing
 # - use generic trick to time total of a program-run
 #
@@ -42,7 +44,32 @@ def tmr_durat ():
   durat = ( datetime.now() - g_tmr_durat ).total_seconds()
   return  durat
 
+def tmr_spin ( spin_s: float ):
+
+  start_t = datetime.now().timestamp()        # time.time()
+  end_t   = start_t + spin_s 
+
+  while datetime.now().timestamp() < end_t:   # time.time() < end_t:
+    curr_t = datetime.now().timestamp()       # time.time()
+    _ = curr_t * curr_t
+
+  end_t   =  datetime.now().timestamp         # time.time()
+
+  # return ( end_t - start_t )                #  with just time.time()
+  return ( datetime.now().timestamp() - start_t )      # didnt need timestamp-funciton with just time.time()
+
 # ----- timers defined ---- 
+
+_hidden_var = 42  # This is "private" by convention
+
+def pf_get():
+  global _hidden_var
+  return _hidden_var
+
+def pf_set():
+  global _hidden_var
+  _hidden_var = _hidden_var + 1 
+  return _hidden_var
 
 # test code, uncomment if testing
 
@@ -56,7 +83,7 @@ if __name__ == '__main__':
 
   time.sleep ( 1 ) 
   print ( '\n --- dur1   : ', tmr_durat(), '\t\t, this measured the stopwatch-time, 1sec?.' )
-  print (   ' --- tot1    : ', tmr_total()  , '\t\t, this measured the total time since start, just over 1sec?' )
+  print (   ' --- tot1   : ', tmr_total(), '\t\t, this measured the total time since start, just over 1sec?' )
 
 # print ( ' --- total3 : ', tmr_total() )
 # print ( ' --- dur3   : ', tmr_durat() )
@@ -66,8 +93,23 @@ if __name__ == '__main__':
 # time.sleep ( 1 ) 
 # print ( '\n --- total4 : ', tmr_total() )
 # print (   ' --- dur    : ', tmr_durat() )
-# time.sleep ( 1) 
-# print ( '\n --- re-set : ', tmr_set() )
-# print (   ' --- dur0   : ', f" = {tmr_durat():9.5f}" )
-# print (   ' --- total5 : ', f" = {tmr_total():9.5f}" )
+  time.sleep ( 1) 
+  print ( '\n --- re-set : ', tmr_set() )
+  print (   ' --- dur0   : ', f" = {tmr_durat():9.5f}" )
+  print (   ' --- total5 : ', f" = {tmr_total():9.5f}" )
+
+  n_spin_sec = 3.14
+  print ( '\n -- check spinning... ' )
+  print (   ' --- re-set : ', tmr_set() )
+  n_ret = tmr_spin ( n_spin_sec ) 
+  print (   ' --- dur    : ', tmr_durat(), '\t\t, this measured the spin_time, ', n_spin_sec, ' sec?... ', n_ret , '.' )
+
+
+  print ( ' -- testing pf -- ' )
+
+  print ( 'pf_get : ', pf_get() ) 
+
+  print ( ' - ' )
+ 
+  print ( 'pf_set : ', pf_set() ) 
 
--- ora_login.py
+++ ora_login.py
@@ -5,11 +5,22 @@
 # todo: 
 # - later: allow un/pwd@host:port\sid to be passed, but prefer dotenv 
 # - how many other self-made includes should I depend on ? 
+# - include ora_aas: find aas and allow throttling/sleep.
+#   if no aas-found, no conn, simply pause bcse no useful work possible
 # 
+
 import    os
+import    time
 import    oracledb
 from      dotenv   import load_dotenv
 
+#
+# functions in this file:
+# ora_logon : use dotenv to get credentials and logon
+# ora_sess_info : get some session stats (since logon of sess..)
+#
+# ora_aas_init  : set startpoint to sample AAS, indication for busy system
+# ora_aas_get   : get AAS since init, include nr of CPUs to relate to
 
 # define function to have any arguments, 
 # this allow to replace existing functions. 
@@ -113,10 +124,101 @@ def ora_sess_info ( the_conn ):
   cur_stats = the_conn.cursor()
   for row in cur_stats.execute ( sql_stats ):
     # p   ( ' stats : ', row[1], ' ', row[0] )
-    print   ( ' ora_sess_info: ', f"{row[1]:10d}  {row[0]}"   )
+    print   ( ' ora_sess_info: ', f"{row[1]}  {row[0]}"   )
 
   return 0 # -- -- -- -- ora_sess_info
 
+
+# -- --  ora_aas -- -- 
+
+# define the global ora_aas variables and functions
+ora_aas_prev_dbtime_ms = 0    # last measured valie, 0 = not initialized yet
+ora_aas_prev_dt_epoch  = 0    # epoch in sec when last db_time was queried
+
+ora_aas_query = """ 
+  select 
+    value                                                   as db_time_ms
+  , (sysdate - TO_DATE('1970-01-01', 'YYYY-MM-DD')) * 86400 as epoch_sec
+  from v$sysstat
+  where name = 'DB time'
+"""
+
+# check for AAS, determine if pause needed, return ms-duration 
+
+def ora_aas_chk ( conn_obj):
+
+  start_ms = time.time() * 1000
+
+  global ora_aas_prev_dbtime_ms
+  global ora_aas_prev_epoch_s
+
+  load_dotenv()
+
+  # get env ... (what if not in env?)
+  ora_aas_threshold_pct    = float ( str ( os.getenv ( 'ORA_AAS_THRESHOLD_PCT' ) ) )
+  ora_aas_pause_sec        = int   ( str ( os.getenv ( 'ORA_AAS_PAUSE_SEC'     ) ) )
+
+  print ( ' ora_aas : from env, thrshld: ', ora_aas_threshold_pct, ', ora_aas_pause: ', ora_aas_pause_sec )
+
+  # get the values from database..
+  cur_aas = conn_obj.cursor()
+  for row in cur_aas.execute ( ora_aas_query ):
+    
+    # print   ( ' ora_aas : info found epoch and dbtime: ', f"{row[1]}  {row[0]}"   )
+    # should only be 1 row...
+ 
+    dbtime_ms = int ( row[0] )
+    epoch_s   = int ( row[1] )
+  
+  # end for-cursor, should only be 1
+
+  print ( ' ora_aas : dbtime: ', dbtime_ms, ', epoch: ', epoch_s )
+
+  # when not ini, just pause
+  # else, if already init: determine if pause wanted..
+
+  if ora_aas_prev_dbtime_ms == 0:
+
+    print ( ' ora_aas: initiated and doing pause ', ora_aas_pause_sec, ' sec.' ) 
+    time.sleep ( ora_aas_pause_sec )  
+
+  else: # no init, but
+
+    # calculate aas percentage (explicit calcs, allow debug)
+    delta_dbtime_ms = dbtime_ms - ora_aas_prev_dbtime_ms
+    delta_epoch_s   = epoch_s   - ora_aas_prev_epoch_s
+    aas_pct = ( 100 * 
+         ( delta_dbtime_ms      ) 
+       / ( delta_epoch_s * 1000 ) 
+     )
+
+    print ( ' ora_aas : delta_dbtime_ms =', delta_dbtime_ms )
+    print ( ' ora_aas : delta_epoch_s   =', delta_epoch_s )
+    print ( ' ora_aas : aas_pct         =', aas_pct )
+
+    if aas_pct > ora_aas_threshold_pct:
+
+      print ( ' ora_aas : need pause - yes ' )
+      time.sleep ( ora_aas_pause_sec ) 
+
+    else:
+
+      print ( ' ora_aas : dont need pause  - no' ) 
+
+    # endif, needed pause or not
+
+  # end if-else, inti or check 
+
+  # save the prev values
+  ora_aas_prev_dbtime_ms = dbtime_ms
+  ora_aas_prev_epoch_s   = epoch_s
+  
+  end_ms = time.time() * 1000
+  duration_ms = end_ms - start_ms
+
+  print ( ' ora_aas : done, return duration: ', duration_ms )
+
+  return duration_ms
 
 # ---- some  test code below... ---- 
 
@@ -135,6 +237,12 @@ if __name__ == '__main__':
   ora_conn = ora_logon ()
 
   print ()
+  print ( ' ---- if connected, init ora_aas ----- ' )
+  print ()
+
+  ora_aas_chk ( ora_conn )
+
+  print ()
   print ( ' ---- if connected, test a query ----- ' )
   print ()
 
@@ -143,7 +251,14 @@ if __name__ == '__main__':
     print ( ' ora_logon:', row ) 
   
   print ()
-  print ( ' ---- ora_logon: tested with count * for user_objects ---- ' ) 
+  print ( ' ---- ora_logon: tested count *  user_objects, do 1 more aas ---- ' ) 
+  print ()
+
+  time.sleep( 30 )
+  ora_aas_chk ( ora_conn )
+
+  print ()
+  print ( ' ---- ora_logon: check the stats, for current sesion ---- ' ) 
   print ()
 
   ora_sess_info ( ora_conn )
